6186ffb8623500027612fb617993567e743fac25
duy <duy@mail.com>
2024-11-21 15:49:18 +0700

    Updated LaTeXSwiftUI

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
     .package(url: "https://github.com/gonzalezreal/NetworkImage", from: "6.0.0"),
     .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.10.0"),
     .package(url: "https://github.com/swiftlang/swift-cmark", from: "0.4.0"),
-    .package(url: "https://github.com/tranleduy2000/LaTeXSwiftUI.git", revision: "10624752f736c73fee0e04713771c2a7263bb35e"),
+    .package(url: "https://github.com/tranleduy2000/LaTeXSwiftUI.git", revision: "a0406a3caa68c9589127eca058fac44c181db31c"),
     //.package(path: "../LaTeXSwiftUI"),
   ],
   targets: [
